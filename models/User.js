f683cac24b65005fc966a885de700f832e83ab17
Lewis Horwood <horwood.lewis@gmail.com>
2018-11-05 10:32:43 +0900

    add User model

---
+++ models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    username: String,
+    exercises: [
+        {
+            description: String,
+            duration: Number,
+            date: Date
+        }
+    ]
+});
+
+const User = mongoose.model('User', schema);
+
+export default User;
