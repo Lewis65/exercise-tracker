f862c8f89ec4a2d67c5e21372a5a49046e110a9c
Lewis Horwood <horwood.lewis@gmail.com>
2018-11-07 21:05:36 +0900

    add reference to userId

--- models/Exercise.js
+++ models/Exercise.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
     description: String,
     duration: Number,
-    date: Date
+    date: Date,
+    userId: String
 }, {timestamps: true});
 
 const Exercise = mongoose.model('Exercise', schema);
